b96538e7c444626b035f2a5dffa437e2200a7aec
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2022-10-12 10:23:55 +0300

    Done with js specials and fundamentals

---
+++ 02_fundamentals/18_js_specials/script.js
@@ -0,0 +1,12 @@
+// console.log("Hello");
+// console.log("World!");
+
+// const printDouble = number => console.log(number * 2);
+
+// [2, 3, 4].forEach(printDouble);
+
+let userName = prompt("Your name?", "Alice");
+let isTeaWanted = confirm("Do you want some tea?");
+
+alert(`Visitor: ${userName}`);
+alert(`Tea wanted: ${isTeaWanted}`);
