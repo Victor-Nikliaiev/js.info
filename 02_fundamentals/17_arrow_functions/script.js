190981686a6f1ddddd7866ba6ba60f0c1b5cc646
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2022-10-12 09:56:22 +0300

    Done with arrow funcs

---
+++ 02_fundamentals/17_arrow_functions/script.js
@@ -0,0 +1,36 @@
+// let sum = (a, b) => a + b;
+
+// console.log(sum(2, 3));
+
+// let double = number => number * 2;
+
+// console.log(double(3));
+
+// let sayHi = () => console.log("Hi!");
+
+// sayHi();
+
+// let age = 34;
+
+// let welcome =
+// 	age < 18 ? () => console.log("Hello!") : () => console.log("Greetings!");
+
+// welcome();
+
+// let sum = (a, b) => {
+// 	let result = a + b;
+// 	return result;
+// };
+
+// console.log(sum(3, 5));
+
+// function ask(question, yes, no) {
+// 	if (confirm(question)) yes();
+// 	else no();
+// }
+
+// ask(
+// 	"Do you agree",
+// 	() => alert("You agreed."),
+// 	() => alert("You canceled.")
+// );
