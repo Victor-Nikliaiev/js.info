4b12cfde1d20ba9dd8edd41b9765029ae4ddbca7
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2022-11-06 13:02:37 +0200

    Done with old var.

---
+++ src/06_advanced_working_with_funcs/04_old_var/script.js
@@ -0,0 +1,67 @@
+import alert from "alert";
+
+// if (true) {
+//   let test = "test";
+// }
+
+// alert(test);
+
+// for (var i = 0; i < 10; i++) {
+//   var one = 1;
+// }
+
+// alert(i);
+// alert(one);
+
+// function sayHi() {
+//   if (true) {
+//     var phrase = "Hello";
+//   }
+
+//   alert(phrase);
+// }
+
+// sayHi();
+// alert(phrase);
+
+// var test = "test";
+// var test = "another test";
+
+// alert(test);
+
+// function sayHi() {
+//   phrase = "hello";
+//   console.log(phrase);
+//   var phrase;
+// }
+
+// sayHi();
+
+// function func() {
+//   if (false) {
+//     var test = false;
+//     test = true;
+//   }
+
+//   console.log(test);
+// }
+
+// func();
+
+// function sayHi() {
+//   var phrase;
+//   phrase = "5";
+//   console.log(phrase);
+// }
+
+// sayHi();
+
+// (function () {
+//   var test = "Hi";
+//   alert(test);
+// })();
+// console.log(test);
+
+!(function () {
+  alert("TEST");
+})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import "./06_advanced_working_with_funcs/03_closure/script.js";
+import "./06_advanced_working_with_funcs/04_old_var/script.js";
