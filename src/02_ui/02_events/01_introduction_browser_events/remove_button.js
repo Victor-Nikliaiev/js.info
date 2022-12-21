ade6ba3069049a49ac1cfc8c1fe010a98e0022e4
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2022-12-21 13:02:56 +0200

    Done with introduction to browser events

--- src/02_ui/02_events/01_introduction_browser_events/remove_button.js
+++ src/02_ui/02_events/01_introduction_browser_events/remove_button.js
@@ -1,1 +1,36 @@
-console.log("TTT");
+// One variant
+
+// const removeButton = document.createElement("button");
+// removeButton.innerText = "[x]";
+// removeButton.classList.add("remove-button");
+// removeButton.onclick = function () {
+//   console.log("test");
+// };
+
+// const paneList = document.querySelectorAll(".pane");
+// console.log(paneList);
+
+// function clickHandler() {
+//   const parentPane = this.parentElement;
+//   parentPane.remove();
+// }
+
+// paneList.forEach(paneElement => {
+//   const clonedElement = removeButton.cloneNode(true);
+//   clonedElement.addEventListener("click", clickHandler);
+//   paneElement.append(clonedElement);
+// });
+
+// Another variant
+
+const paneList = document.querySelectorAll(".pane");
+
+paneList.forEach(paneElement => {
+  paneElement.insertAdjacentHTML(
+    "afterbegin",
+    '<button class="remove-button">[x]</button>'
+  );
+  paneElement.firstElementChild.onclick = function () {
+    this.parentElement.remove();
+  };
+});
